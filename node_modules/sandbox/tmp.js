3e6e8a3b6065c9686fe0c441a661543e5ade4598
yannick mathey <hello@yannickmathey.com>
2017-06-20 09:55:50 +0200

    sync

--- node_modules/sandbox/tmp.js
+++
@@ -1,8 +0,0 @@
-var Sandbox = require("./lib/sandbox")
-  , s = new Sandbox()
-
-// Example 10 - Console Log
-s.run( "var x = 5; console.log(x * x); console.log('pewpew');  x", function( output ) {
-  console.log( "Example 10: " + output.console + "\n" )
-})
-
